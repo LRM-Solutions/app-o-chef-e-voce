6f0cf28c749fff6231873645389580fb56ccd203
LRM Solutions <contato@lrmsolutions.com.br>
2026-09-03 15:58:42 -0300

    fix: restaura importacao de getUserPedidos em MeusPedidosScreen

--- src/screens/MeusPedidosScreen.js
+++ src/screens/MeusPedidosScreen.js
@@ -11,6 +11,7 @@ import {
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import { getUserPedidos } from "../api/pedidosApi";
 import { formatarStatusPagamento, formatarStatusPedido, cancelarPedido } from "../api/pedidoDetalhesApi";
 import { createTextStyle, createButtonStyle } from "../utils/theme";
 import { useTheme } from "../utils/ThemeContext";
